f8080c3894e3f3ec8fbaac1f8cccf0f3db139723
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-07-14 14:17:37 +0700

    respond multiple PING

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
-use std::io::Write;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Write, Read,};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,13 +12,38 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write("+PONG\r\n".as_bytes()).unwrap();
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf = [0; 512];
+    loop {
+        let bytes_read = stream.read(&mut buf).expect("Failed to read from client");
+        let req = match String::from_utf8(buf[..bytes_read].into()) {
+            Ok(req) => req,
+            Err(e) => {
+                eprintln!("failed to parse request as utf8: {e}");
+                return;
+            }
+        };
+
+        println!("{}", req);
+        if req.contains("PING") {
+            stream.write("+PONG\r\n".as_bytes()).unwrap();
+        }
+
+        if bytes_read == 0 {
+            return;
+        }
+
     }
 }
